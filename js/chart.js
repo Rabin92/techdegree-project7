fc0daf07024bd11a031ca23c0985b46a7fba67fe
Rabin Gharti Magar <rabin_mgr@live.com>
2020-08-13 22:21:47 +0100

    modified files

--- js/chart.js
+++ js/chart.js
@@ -1,8 +1,57 @@
-/* ******************************** BAR CHART ******************************* */
-
-// Select myChart and store it in a variable
+// Select elements
 let barChart = document.querySelector("#js-barChart").getContext("2d");
 let donutChart = document.querySelector("#js-donutChart").getContext("2d");
+let lineChart = document.querySelector("#js-lineChart").getContext("2d");
+
+/* ******************************* LINE CHART ******************************* */
+lineChart = new Chart(lineChart, {
+  type: "line",
+
+  data: {
+    labels: [
+      "16-22",
+      "23-29",
+      "30-5",
+      "6-12",
+      "13-19",
+      "20-26",
+      "27-3",
+      "4-10",
+      "11-17",
+      "18-24",
+      "25-31",
+    ],
+
+    datasets: [
+      {
+        backgroundColor: "rgba(0, 0, 0, 0.1)",
+        borderColor: "#7477BF",
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      },
+    ],
+  },
+  // Configuration options
+  options: {
+    responsive: true,
+    responsiveAnimationDuration: 0,
+    maintainAspectRatio: false,
+
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+    legend: {
+      display: false,
+    },
+  },
+});
+
+/* ******************************** BAR CHART ******************************* */
 
 // Create a chart
 barChart = new Chart(barChart, {
