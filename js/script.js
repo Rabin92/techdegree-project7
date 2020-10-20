ca8f92476fe0de178fcbec9585ee97126936fe94
Rabin Gharti Magar <rabin_mgr@live.com>
2020-10-20 15:10:56 +0100

    File updated

--- js/script.js
+++ js/script.js
@@ -1,46 +1,60 @@
 /* *************************** ALERT NOTIFICATIONS ************************** */
-const notifications = document.querySelector("#js-notifications");
-const iconBell = document.querySelector("#js-icon-bell");
-const iconAlert = document.querySelector("#js-icon-red");
-const notificationsMsg = document.querySelector("#js-notifications-msg ul")
+const notifications = document.querySelector('#js-notifications');
+const iconBell = document.querySelector('#js-icon-bell');
+const iconAlert = document.querySelector('#js-icon-red');
+const notificationsMsg = document.querySelector('#js-notifications-msg ul')
   .children;
+const clearAll = document.querySelector('#js-notifications-msg').children;
+const removeNotifications = document.querySelector('.clear-not');
 
-// Hide notification
-notifications.style.display = "none";
+// Display
+const display = (element, style) => (element.style.display = style);
+// Border
+const border = (element, style) => (element.style.border = style);
+// Opacity
+const opacity = (element, style) => (element.style.opacity = style);
 
 // Adding click event on a Bell icon
-iconBell.addEventListener("click", () => {
-  if (notifications.style.display === "none") {
-    notifications.style.display = "block";
+iconBell.addEventListener('click', () => {
+  if (notifications.style.display === 'none') {
+    display(notifications, 'block');
   } else {
-    notifications.style.display = "none";
+    display(notifications, 'none');
   }
 });
 
-// Remove notifications when the user clicks on 'x'
-notifications.addEventListener("click", (e) => {
+// Remove notifications when the user clicks on 'x' or clear all
+notifications.addEventListener('click', e => {
   const theTarget = e.target;
-  if (theTarget.tagName === "SPAN") {
+  if (theTarget.tagName === 'SPAN') {
     theTarget.parentNode.remove();
+  } else if (theTarget.tagName === 'P') {
+    removeNotifications.parentNode.remove();
   }
 
   /* 
-   If notifications is less than 1, remove alert icon
-   then display 'No new message alert'.
+   If notifications is < 2, OR
+   if clearAll length is < 1, 
+   remove alert icon and
+   then display 'No recent notifications'.
   */
 
-  if (notificationsMsg.length < 1) {
-    iconAlert.style.display = "none";
+  if (notificationsMsg.length < 2 || clearAll.length < 1) {
+    display(iconAlert, 'none');
+    display(removeNotifications, 'none');
 
-    const notificationsMsg = document.querySelector("#js-notifications-msg ul");
-    const newEl = document.createElement("LI");
-    newEl.textContent = `No recent notifications`;
-    notificationsMsg.appendChild(newEl);
+    const notificationsMsg = document.querySelector('#js-notifications-msg');
+    const ulEl = document.createElement('UL');
+    const liEl = document.createElement('LI');
+
+    liEl.textContent = `No recent notifications`;
+    notificationsMsg.appendChild(ulEl);
+    ulEl.appendChild(liEl);
   }
 });
 
 /* ****************************** ALERT BANNER ****************************** */
-const alertBanner = document.querySelector("#js-alert");
+const alertBanner = document.querySelector('#js-alert');
 
 alertBanner.innerHTML = `
   <div class="alert-banner">
@@ -50,70 +64,75 @@ alertBanner.innerHTML = `
 `;
 
 // Event listener added
-alertBanner.addEventListener("click", (e) => {
+alertBanner.addEventListener('click', e => {
   const element = e.target;
-  if (element.classList.contains("alert-banner-close")) {
-    alertBanner.style.display = "none";
+  opacity(element.parentNode.parentNode, '0');
+
+  if (element.classList.contains('alert-banner-close')) {
+    setTimeout(() => {
+      display(alertBanner, 'none');
+    }, 600);
   }
 });
 
 /* ***************************** FORM VALIDATION **************************** */
-const form = document.querySelector("#js-form");
-const userName = document.querySelector("#js-name");
-const userMsg = document.querySelector("#js-message");
-const inputError = document.querySelector("#js-input-error");
-const textAreaError = document.querySelector("#js-textarea-error");
-const success = document.querySelector("#js-success");
+const form = document.querySelector('#js-form');
+const userName = document.querySelector('#js-name');
+const userMsg = document.querySelector('#js-message');
+const inputError = document.querySelector('#js-input-error');
+const textAreaError = document.querySelector('#js-textarea-error');
+const success = document.querySelector('#js-success');
 
-form.addEventListener("click", (e) => {
+form.addEventListener('click', e => {
   const theTarget = e.target;
-  if (theTarget.tagName === "BUTTON") {
-    if (userName.value === "") {
+  if (theTarget.tagName === 'BUTTON') {
+    if (userName.value === '') {
       inputError.innerHTML = `<p class="js-error">This field is required!</p>`;
-      userName.style.border = "1px solid red";
+      border(userName, '1px solid red');
     } else {
-      inputError.innerHTML = "";
-      userName.style.border = "1px solid black";
+      inputError.innerHTML = '';
+      border(userName, '1px solid black');
     }
-    if (userMsg.value === "") {
+    if (userMsg.value === '') {
       textAreaError.innerHTML = `<p class="js-error">This field is required!<p/>`;
-      userMsg.style.border = "1px solid red";
+      border(userMsg, '1px solid red');
     } else {
-      textAreaError.innerHTML = "";
-      userMsg.style.border = "1px solid black";
+      textAreaError.innerHTML = '';
+      border(userMsg, '1px solid black');
     }
-    if (userName.value !== "" && userMsg.value !== "") {
+    if (userName.value !== '' && userMsg.value !== '') {
       success.innerHTML = `<p class="js-close">Message successfully sent to: ${userName.value}<span class="js-close-alert">&times;</span></p>`;
     } else {
-      success.innerHTML = "";
+      success.innerHTML = '';
     }
   }
 });
 
 // Click on 'x' to close success alert message.
-document.querySelector("#js-form").addEventListener("click", (e) => {
+document.querySelector('#js-form').addEventListener('click', e => {
   const theTarget = e.target;
-  if (theTarget.tagName === "SPAN") {
+  if (theTarget.tagName === 'SPAN') {
     theTarget.parentNode.remove();
   }
 });
 
 /* *************************** AUTOCOMPLETE SEARCH ************************** */
-const listNames = document.querySelector("#js-list-names ul");
-listNames.style.display = "none";
+const listNames = document.querySelector('#js-list-names ul');
+display(listNames, 'none');
 
 // Store members name in an array
 const memberNames = [
-  "Victoria Chambers",
-  "Dale Byrd",
-  "Dan Oliver",
-  "Dawn Wood",
+  'Victoria Chambers',
+  'Dale Byrd',
+  'Dan Oliver',
+  'Dawn Wood',
+  'Rabin Gharti Magar',
 ];
 
 // Create "LI" element and append it to "listNames"
 for (let i = 0; i < memberNames.length; i++) {
   const names = memberNames[i];
-  const newEl = document.createElement("LI");
+  const newEl = document.createElement('LI');
   newEl.textContent = names;
   listNames.appendChild(newEl);
 }
@@ -122,64 +141,64 @@ const displayMatchedNames = () => {
   const inputValue = userName.value.toUpperCase();
 
   if (inputValue.length < 1) {
-    listNames.style.display = "none";
+    display(listNames, 'none');
   } else {
-    listNames.style.display = "block";
+    display(listNames, 'block');
   }
 
   // Iterate through "<li>" members list.
-  const memberLists = listNames.querySelectorAll("li");
+  const memberLists = listNames.querySelectorAll('li');
   for (let i = 0; i < memberLists.length; i++) {
     const name = memberLists[i].textContent.toUpperCase();
     //  If the member's name stored in <li> matches the value typed in input field, display as block else display it none.
     if (name.includes(inputValue)) {
-      memberLists[i].style.display = "block";
+      display(memberLists[i], 'block');
     } else {
-      memberLists[i].style.display = "none";
+      display(memberLists[i], 'none');
     }
   }
 };
 
 // Call displayMatchedNames function
-userName.addEventListener("input", displayMatchedNames);
+userName.addEventListener('input', displayMatchedNames);
 
 // Place the selected name in the input field
-listNames.addEventListener("click", (e) => {
+listNames.addEventListener('click', e => {
   const selectNames = e.target.textContent;
 
-  const memberLists = document.querySelector("#js-list-names ul").children;
+  const memberLists = document.querySelector('#js-list-names ul').children;
   for (let i = 0; i < memberLists.length; i++) {
     userName.value = selectNames;
-    listNames.style.display = "none";
+    display(listNames, 'none');
   }
 });
 
 /* ****************************** LOCAL STORAGE ***************************** */
-const notificationsBtn = document.querySelector("#js-notifications-btn");
-const profileBtn = document.querySelector("#js-profile-btn");
-const timeZone = document.querySelector("#js-timezone");
-const alertMsg = document.querySelector("#js-alert-msg");
-const saveBtn = document.querySelector("#js-save-button");
-const clearBtn = document.querySelector("#js-clear-button");
+const notificationsBtn = document.querySelector('#js-notifications-btn');
+const profileBtn = document.querySelector('#js-profile-btn');
+const timeZone = document.querySelector('#js-timezone');
+const alertMsg = document.querySelector('#js-alert-msg');
+const saveBtn = document.querySelector('#js-save-button');
+const clearBtn = document.querySelector('#js-clear-button');
 
 // Listen to 'click' on save button
-saveBtn.addEventListener("click", () => {
+saveBtn.addEventListener('click', () => {
   const email = notificationsBtn.checked;
   const profile = profileBtn.checked;
 
   //  Add key and value to localStorage.
   if (email) {
-    localStorage.setItem("email", true);
+    localStorage.setItem('email', true);
   } else {
-    localStorage.setItem("email", false);
+    localStorage.setItem('email', false);
   }
   if (profile) {
-    localStorage.setItem("profile", true);
+    localStorage.setItem('profile', true);
   } else {
-    localStorage.setItem("profile", false);
+    localStorage.setItem('profile', false);
   }
 
-  localStorage.setItem("timezone", timeZone.selectedIndex);
+  localStorage.setItem('timezone', timeZone.selectedIndex);
 
   // Display message
   alertMsg.innerHTML = `<p class="js-close">Settings successfully saved!<span class="js-close-alert">&times;</span></p>`;
@@ -191,25 +210,25 @@ saveBtn.addEventListener("click", () => {
 */
 const load = () => {
   // Email setting
-  if (localStorage.getItem("email") === "true") {
+  if (localStorage.getItem('email') === 'true') {
     notificationsBtn.checked = true;
   } else {
     notificationsBtn.checked = false;
   }
   // Profile Setting
-  if (localStorage.getItem("profile") === "true") {
+  if (localStorage.getItem('profile') === 'true') {
     profileBtn.checked = true;
   } else {
     profileBtn.checked = false;
   }
   // Timezone setting
-  if (localStorage.getItem("timezone") === "1") {
+  if (localStorage.getItem('timezone') === '1') {
     timeZone.selectedIndex = 1;
-  } else if (localStorage.getItem("timezone") === "2") {
+  } else if (localStorage.getItem('timezone') === '2') {
     timeZone.selectedIndex = 2;
-  } else if (localStorage.getItem("timezone") === "3") {
+  } else if (localStorage.getItem('timezone') === '3') {
     timeZone.selectedIndex = 3;
-  } else if (localStorage.getItem("timezone") === "4") {
+  } else if (localStorage.getItem('timezone') === '4') {
     timeZone.selectedIndex = 4;
   }
 };
@@ -218,15 +237,15 @@ const load = () => {
   Listen to 'click' on cancel button. 
   Reset the settings back to default.
 */
-clearBtn.addEventListener("click", () => {
-  if (localStorage.getItem("email") === "true") {
-    localStorage.setItem("email", (notificationsBtn.checked = false));
+clearBtn.addEventListener('click', () => {
+  if (localStorage.getItem('email') === 'true') {
+    localStorage.setItem('email', (notificationsBtn.checked = false));
   }
-  if (localStorage.getItem("profile") === "true") {
-    localStorage.setItem("profile", (profileBtn.checked = false));
+  if (localStorage.getItem('profile') === 'true') {
+    localStorage.setItem('profile', (profileBtn.checked = false));
   }
-  if (localStorage.getItem("timezone") > "0") {
-    localStorage.setItem("timezone", (timeZone.selectedIndex = "0"));
+  if (localStorage.getItem('timezone') > '0') {
+    localStorage.setItem('timezone', (timeZone.selectedIndex = '0'));
   }
 
   // Display message
@@ -234,9 +253,9 @@ clearBtn.addEventListener("click", () => {
 });
 
 // Click on 'x' to close settings alert message.
-document.querySelector(".close").addEventListener("click", (e) => {
+document.querySelector('.close').addEventListener('click', e => {
   const theTarget = e.target;
-  if (theTarget.className === "js-close-alert") {
+  if (theTarget.className === 'js-close-alert') {
     theTarget.parentNode.remove();
   }
 });
